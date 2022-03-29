e2f3c7f6f86de9216893a96ee34f37a46e479b06
jesus7294 <80054833+jesus7294@users.noreply.github.com>
2022-03-29 13:48:07 -0600

    Se agrega Programas
    Se agrega Programas

---
+++ Practica1/Practica1C#/Calcu/Clases/Div.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcu.Clases
+{
+    internal class Div
+    {
+        public double Dividir(double N1, Double N2)
+        {
+            double d;
+            d = N1 / N2;
+            return d;
+        }
+    }
+}
