b45b8ec3f4ba7bc89566a516798c186ccb2a2d40
jesus7294 <80054833+jesus7294@users.noreply.github.com>
2022-03-29 00:31:13 -0600

    Add files via upload

---
+++ Jesus Anegl Balam Sanchez/Practica1/Practica1_C#/Calcu/Clases/Mult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcu.Clases
+{
+    internal class Mult
+    {
+        public double Multiplicar(double N1, Double N2)
+        {
+            double m;
+            m = N1 * N2;
+            return m;
+        }
+    }
+}
---
+++ Jesus Anegl Balam Sanchez/Practica1/Practica1_C#/Calcu/Clases/Suma.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcu.Clases
+{
+    class Suma
+    {
+        public double Sumar(double N1, Double N2) 
+        {
+            double sum;
+            sum = N1 + N2;
+            return sum;
+        }
+        
+    }
+}
